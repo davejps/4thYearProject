de8757b886734c06bb0d6d18bcb46b83f0e401b1
ciascu93 <d00148492@student.dkit.ie>
2016-04-11 15:26:10 +0100

    Second iteration website

---
+++ statSportsDataV2.sql
@@ -0,0 +1,39 @@
+drop database if exists `statssportsdata`;
+CREATE DATABASE IF NOT EXISTS `statssportsdata`;
+
+USE `statssportsdata`;
+
+create table if not exists sessions
+(
+	sessionId INT not null,
+	sessionDate date not null,
+    startTime time not null,
+	endTime time not null,
+	bottomLeftLat DOUBLE not null,
+	bottomLeftLong DOUBLE not null,
+	bottomRightLat DOUBLE not null,
+	bottomRightLong DOUBLE not null,
+	topLeftLat DOUBLE not null,
+	topLeftLong DOUBLE not null,
+	PRIMARY KEY(`sessionId`)
+)ENGINE=InnoDB;
+
+create table if not exists gps
+(
+    GPSTime Time  not null,
+	latitude DOUBLE not null,
+	longitude DOUBLE  not null,
+	speed DOUBLE  not null,
+	sessionId INT not null,
+	FOREIGN KEY(`sessionId`) REFERENCES `sessions`(`sessionId`)ON DELETE CASCADE ON UPDATE CASCADE
+)ENGINE=InnoDB;
+
+create table if not exists accelerometer
+(
+    GPSTime Time not null,
+	x DOUBLE not null,
+	z DOUBLE not null,
+	y DOUBLE not null,
+	sessionId INT not null,
+	FOREIGN KEY(`sessionId`) REFERENCES `sessions`(`sessionId`)ON DELETE CASCADE ON UPDATE CASCADE
+)ENGINE=InnoDB;
